2cd34fb9708e8d582bcde6041ec4801acfc2f4d0
josue-alva <josue.alvarado@tec.mx>
2025-10-02 23:54:02 -0600

    eslint

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,8 @@
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
-    // Permite que el build continúe aunque haya errores de ESLint
     ignoreDuringBuilds: true,
   },
 };
 
 module.exports = nextConfig;
-
-export default nextConfig;
